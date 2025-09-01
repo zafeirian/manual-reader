2deda15361c967fb9d593a1e8fd3442a0118af84
zafeirian <zafosnikos@gmail.com>
2025-09-01 20:50:58 +0300

    Created lifespan function. Added the embedding function and the creation of the temporary upload directory

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
 from typing import Annotated
 from pydantic import BaseModel
-from langchain_community.document_loaders import PyPDFLoader
-import re
-
+from create_vdb import generate_vdb
 from dotenv import load_dotenv
-app = FastAPI(title='Manual Reader')
+from langchain_openai import OpenAIEmbeddings
+import shutil, tempfile, os
+from pathlib import Path
+from contextlib import asynccontextmanager
 
-load_dotenv()
+
 CHROMA_PATH='chroma'
 EMBED_MODEL='text'
 LLM_MODEL = 'gpt-4o-mini'
 
-def load_documents(files):
-    all_docs = []
-    for file in files:
-        loader=PyPDFLoader(file)
-        documents=loader.load()
-        for doc in documents:
-            doc.page_content = re.sub(r"\s+", " ", doc.page_content).strip()
-        all_docs.extend(documents)
-    return all_docs
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    load_dotenv()
+    api_key = os.getenv('OPENAI_API_KEY')
+    if not api_key:
+        raise RuntimeError("OPENAI_API_KEY not set.")
+
+    embedding_function = OpenAIEmbeddings(
+    model=EMBED_MODEL,
+    api_key=api_key,
+    chunk_size=64,
+    )
+
+    upload_dir = tempfile.mkdtemp(prefix='uploads_')
+
+    app.state.upload_dir = upload_dir
+    app.state.embedding_function = embedding_function
+
+    try:
+        yield
+    finally:
+        app.state.embedding_function = None
+        shutil.rmtree(upload_dir, ignore_errors=True)
+
+
+app = FastAPI(title='Manual Reader', lifespan=lifespan)
 
 
 @app.get("/health")
@@ -29,12 +47,12 @@ def health():
 
 @app.post("/upload")
 async def upload(files: list[UploadFile] = File(...)):
-    all_docs = []
-    for pdf_file in files:
-        loader = PyPDFLoader(pdf_file)
-        documents =loader.load()
-        for doc in documents:
-            doc.page_content = re.sub(r"\s+", " ", doc.page_content).strip()
-        all_docs.extend(documents)
+    tmp_paths: list[str] = []
+    documents = generate_vdb(files, embedding_function, CHROMA_PATH)
+    if len(documents) > 0:
+        return {"VDB created": True}
+    else:
+        raise HTTPException(status_code=500, detail='VDB not created.')
+
 
 
